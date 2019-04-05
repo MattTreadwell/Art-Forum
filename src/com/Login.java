1454ca4a67aa2bb8752f7443f93b8613887411b7
Matthew Treadwell <Treadwell@inventati.org>
2019-04-05 13:16:31 -0700

    Work on database integration, create database test skeleton. Running into class errors in indexDynamic.jsp

--- src/com/Login.java
+++ src/com/Login.java
@@ -11,10 +11,13 @@ import java.io.IOException;
 @WebServlet(name = "Login", value = "/Login")
 public class Login extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        // TODO finish this
+        // Should check check the user's password and username if they are in the database
+        // On fail: return them to login.jsp with attribute 'errormsg' as the error
+        // On success: return user to index.jsp with 'username' attribute set as their username
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        // DO NOT ALLOW USERS TO LOGIN VIA POST (leave blank)
     }
 }
