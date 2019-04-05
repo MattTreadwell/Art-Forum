08411515085dd02690f1712ec29593c9c85ef7e1
Chen Sheng <shen535@usc.edu>
2019-04-05 16:08:05 -0700

    login and register servlet

--- src/com/Login.java
+++ src/com/Login.java
@@ -1,10 +1,14 @@
 package com;
 
+import DB_util.Database;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 
@@ -15,6 +19,38 @@ public class Login extends HttpServlet {
         // Should check check the user's password and username if they are in the database
         // On fail: return them to login.jsp with attribute 'errormsg' as the error
         // On success: return user to index.jsp with 'username' attribute set as their username
+        String username = request.getParameter("username");
+        if(username == null||username.trim().length()==0)
+        {
+            request.setAttribute("errormsg","Username is blank.");
+            RequestDispatcher dispatch = getServletContext().getRequestDispatcher("/login.jsp");
+            dispatch.forward(request, response);
+            return;
+        }
+        username = username.trim();
+        String password = request.getParameter("password");
+        if(password == null||password.trim().length()==0)
+        {
+            request.setAttribute("errormsg","Password is blank.");
+            RequestDispatcher dispatch = getServletContext().getRequestDispatcher("/login.jsp");
+            dispatch.forward(request, response);
+            return;
+        }
+        password = password.trim();
+        HttpSession session = request.getSession();
+        Database database = (Database) session.getAttribute("database");
+        String mess = database.Validate(username,password.hashCode());
+        if(!mess.equals(Database.Success))
+        {
+            request.setAttribute("errormsg",mess);
+            RequestDispatcher dispatch = getServletContext().getRequestDispatcher("/login.jsp");
+            dispatch.forward(request, response);
+            return;
+        }
+        request.setAttribute("username",username);
+        RequestDispatcher dispatch = getServletContext().getRequestDispatcher("/index.jsp");
+        dispatch.forward(request, response);
+        return;
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- src/com/Register.java
+++ src/com/Register.java
@@ -1,10 +1,14 @@
 package com;
 
+import DB_util.Database;
+import DB_util.user;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "Register", value = "/Register")
@@ -14,6 +18,51 @@ public class Register extends HttpServlet {
         // Should check if both 'password' and 'checkpassword' match, and if the username is blank
         // On fail: return them to register.jsp with attribute 'errormsg' as the error
         // On success: return user to index.jsp with 'username' attribute set as their username and add the user to the database
+
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String checkpassword = request.getParameter("checkpassword");
+        if(username == null||username.trim().length()==0)
+        {
+            request.setAttribute("errormsg","Username is blank.");
+            RequestDispatcher dispatch = getServletContext().getRequestDispatcher("/register.jsp");
+            dispatch.forward(request, response);
+            return;
+        }
+        username = username.trim();
+        if(password == null||password.trim().length()==0||checkpassword == null||checkpassword.trim().length()==0)
+        {
+            request.setAttribute("errormsg","Password is blank.");
+            RequestDispatcher dispatch = getServletContext().getRequestDispatcher("/register.jsp");
+            dispatch.forward(request, response);
+            return;
+        }
+        password = password.trim();
+        checkpassword = checkpassword.trim();
+        if(!password.equals(checkpassword))
+        {
+            request.setAttribute("errormsg","Passwords do not match.");
+            RequestDispatcher dispatch = getServletContext().getRequestDispatcher("/register.jsp");
+            dispatch.forward(request, response);
+            return;
+        }
+
+        user user = new user(username,password.hashCode(),"",0,0);
+        HttpSession session = request.getSession();
+        Database database = (Database) session.getAttribute("database");
+        String mess = database.addUser(user);
+        if(!mess.equals(Database.Success))
+        {
+            request.setAttribute("errormsg",mess);
+            RequestDispatcher dispatch = getServletContext().getRequestDispatcher("/register.jsp");
+            dispatch.forward(request, response);
+            return;
+        }
+        request.setAttribute("username",username);
+        RequestDispatcher dispatch = getServletContext().getRequestDispatcher("/index.jsp");
+        dispatch.forward(request, response);
+        return;
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
